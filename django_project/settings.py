3f6c251b6e0f9cd49d58e4f8937f11fc9ba038cc
Victor <victor.alvarez@mail.utoronto.ca>
2020-07-18 21:11:51 -0400

    Added django-heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-first-django-app-blog.herokuapp.com']
+ALLOWED_HOSTS = ['my-first-django-app-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -148,3 +149,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
